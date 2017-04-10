fcee063cb4e015ebbbfc1025a73dee55a8c9f185
Kirk Burleson <kirk.burleson@yahoo.com>
2017-04-09 20:55:26 -0500

    rewrote instructions

--- freq.js
+++ freq.js
@@ -1,32 +1,3 @@
-/*
-	1) Make a function that takes a string and returns an object that contains the
-	total number of times each letter appears in the string (case matters). For example:
-
-	{
-		a: 3,
-		b: 2,
-		A: 1
-	}
-
-	2) Using the output from the first program, display the frequencies in the console. Each letter
-	should appear on its own line and the frequency should be represented by the '*' character.
-	For example:
-
-	a ***
-	b **
-	A *
-
-	3) Modify the second function so it can display the graph vertically, as well as, horizontally.
-	You should leave a space in between letters so it's easier to read.
-	For example: *Note the extra horizontal spaces for readability
-
-	*
-	*  *
-	*  *  *
-	a  b  A
-
-*/
-
 var freq = (function() {
 	function getFreq(s) {
 		let result = {},
@@ -87,3 +58,4 @@ var freq = (function() {
 
 const s = "kirk loves to program in javascript";
 freq.display(freq.get(s), 0, 2);
+freq.display(freq.get(s), 1, 2);
